79fe3970c64e9215a53e800a133f9aaee07e7460
Pratul Makar <pratulmakar02@gmail.com>
2024-08-29 22:57:31 +0530

    implemented router-dom

---
+++ my-project/public/data/data.js
@@ -0,0 +1,111 @@
+// data.js
+
+export const dsa = [
+  { label: "Analysis of Algorithms" },
+  { label: "Array" },
+  { label: "Linked List" },
+  { label: "Searching Algorithms" },
+  { label: "Stack" },
+  { label: "Sorting Algorithms" },
+  { label: "Queue" },
+  { label: "Graph" },
+];
+
+export const webDevelopment = [
+  { label: "JavaScript" },
+  { label: "HTML" },
+  { label: "CSS" },
+  { label: "ReactJS" },
+  { label: "Node.js" },
+  { label: "Django" },
+  { label: "Frontend Development" },
+  { label: "Backend Development" },
+];
+
+export const aiMlDataScience = [
+  { label: "Artificial Intelligence" },
+  { label: "Machine Learning" },
+  { label: "Data Science" },
+  { label: "Data Analysis" },
+  { label: "Data Visualization" },
+  { label: "Deep Learning" },
+  { label: "Natural Language Processing" },
+  { label: "Computer Vision" },
+];
+
+export const programmingLanguages = [
+  { label: "Python" },
+  { label: "Java" },
+  { label: "C++" },
+  { label: "C" },
+  { label: "R" },
+  { label: "PHP" },
+  { label: "Flutter" },
+  { label: "Golang" },
+];
+
+export const interviewPreparation = [
+  { label: "Company Preparation" },
+  { label: "SDE Interview Programs" },
+  { label: "Competitive Programming" },
+  { label: "Practice Company Questions" },
+  { label: "Aptitude" },
+  { label: "Interview Experiences" },
+  { label: "Puzzles" },
+  { label: "Company Recruitment" },
+];
+
+export const csSubjects = [
+  { label: "Operating System" },
+  { label: "Computer Network" },
+  { label: "DBMS" },
+  { label: "Engineering Mathematics" },
+  { label: "Software Engineering" },
+  { label: "Digital Electronics" },
+  { label: "Computer Design" },
+  { label: "Computer Organization" },
+];
+
+export const databases = [
+  { label: "SQL" },
+  { label: "MySQL" },
+  { label: "MongoDB" },
+  { label: "PostgreSQL" },
+  { label: "PL/SQL" },
+  { label: "SQL CheatSheet" },
+  { label: "SQL Interview Questions" },
+  { label: "MySQL Interview Questions" },
+];
+
+export const devOps = [
+  { label: "GIT" },
+  { label: "Amazon Web Services" },
+  { label: "Kubernetes" },
+  { label: "Docker" },
+  { label: "Microsoft Azure" },
+  { label: "Google Cloud Platform" },
+  { label: "DevOps Roadmap" },
+  { label: "Learn DevOps Online" },
+];
+
+export const tutorials = [
+  { label: "System Design" },
+  { label: "Android" },
+  { label: "Linux" },
+  { label: "Design Pattern" },
+  { label: "Software Testing" },
+  { label: "Product Management" },
+  { label: "Project Management" },
+  { label: "Excel" },
+];
+
+export const gfgSchool = [
+  { label: "Class 9 Study Material" },
+  { label: "Class 10 Study Material" },
+  { label: "Class 11 Study Material" },
+  { label: "Class 12 Study Material" },
+  { label: "Class 9 Notes" },
+  { label: "Class 10 Notes" },
+  { label: "Class 11 Notes" },
+  { label: "Class 12 Notes" },
+];
--- my-project/src/CourseContent.jsx
+++ my-project/src/CourseContent.jsx
@@ -1,123 +1,25 @@
 import React from "react";
-import { useNavigate } from "react-router-dom"; // Import useNavigate hook
+import { useNavigate } from "react-router-dom";
+import {
+  aiMlDataScience,
+  csSubjects,
+  databases,
+  devOps,
+  dsa,
+  gfgSchool,
+  interviewPreparation,
+  programmingLanguages,
+  tutorials,
+  webDevelopment,
+} from "../public/data/data.js"; // Import data from data.js
 import CourseCard from "./components/CourseCard";
 
 const CourseContent = () => {
-  const navigate = useNavigate(); // Hook to handle navigation
-
-  const dsa = [
-    { label: "Analysis of Algorithms" },
-    { label: "Array" },
-    { label: "Linked List" },
-    { label: "Searching Algorithms" },
-    { label: "Stack" },
-    { label: "Sorting Algorithms" },
-    { label: "Queue" },
-    { label: "Graph" },
-  ];
-
-  const webDevelopment = [
-    { label: "JavaScript" },
-    { label: "HTML" },
-    { label: "CSS" },
-    { label: "ReactJS" },
-    { label: "Node.js" },
-    { label: "Django" },
-    { label: "Frontend Development" },
-    { label: "Backend Development" },
-  ];
-
-  const aiMlDataScience = [
-    { label: "Artificial Intelligence" },
-    { label: "Machine Learning" },
-    { label: "Data Science" },
-    { label: "Data Analysis" },
-    { label: "Data Visualization" },
-    { label: "Deep Learning" },
-    { label: "Natural Language Processing" },
-    { label: "Computer Vision" },
-  ];
-
-  const programmingLanguages = [
-    { label: "Python" },
-    { label: "Java" },
-    { label: "C++" },
-    { label: "C" },
-    { label: "R" },
-    { label: "PHP" },
-    { label: "Flutter" },
-    { label: "Golang" },
-  ];
-
-  const interviewPreparation = [
-    { label: "Company Preparation" },
-    { label: "SDE Interview Programs" },
-    { label: "Competitive Programming" },
-    { label: "Practice Company Questions" },
-    { label: "Aptitude" },
-    { label: "Interview Experiences" },
-    { label: "Puzzles" },
-    { label: "Company Recruitment" },
-  ];
-
-  const csSubjects = [
-    { label: "Operating System" },
-    { label: "Computer Network" },
-    { label: "DBMS" },
-    { label: "Engineering Mathematics" },
-    { label: "Software Engineering" },
-    { label: "Digital Electronics" },
-    { label: "Computer Design" },
-    { label: "Computer Organization" },
-  ];
-
-  const databases = [
-    { label: "SQL" },
-    { label: "MySQL" },
-    { label: "MongoDB" },
-    { label: "PostgreSQL" },
-    { label: "PL/SQL" },
-    { label: "SQL CheatSheet" },
-    { label: "SQL Interview Questions" },
-    { label: "MySQL Interview Questions" },
-  ];
-
-  const devOps = [
-    { label: "GIT" },
-    { label: "Amazon Web Services" },
-    { label: "Kubernetes" },
-    { label: "Docker" },
-    { label: "Microsoft Azure" },
-    { label: "Google Cloud Platform" },
-    { label: "DevOps Roadmap" },
-    { label: "Learn DevOps Online" },
-  ];
-
-  const tutorials = [
-    { label: "System Design" },
-    { label: "Android" },
-    { label: "Linux" },
-    { label: "Design Pattern" },
-    { label: "Software Testing" },
-    { label: "Product Management" },
-    { label: "Project Management" },
-    { label: "Excel" },
-  ];
-
-  const gfgSchool = [
-    { label: "Class 9 Study Material" },
-    { label: "Class 10 Study Material" },
-    { label: "Class 11 Study Material" },
-    { label: "Class 12 Study Material" },
-    { label: "Class 9 Notes" },
-    { label: "Class 10 Notes" },
-    { label: "Class 11 Notes" },
-    { label: "Class 12 Notes" },
-  ];
+  const navigate = useNavigate();
 
   // Function to handle "View All" button click
   const handleViewAll = (category) => {
-    navigate(`/courses/${category.toLowerCase().replace(/ /g, "-")}`); // Navigate to the specific category route
+    navigate(`/courses/${category.toLowerCase().replace(/ /g, "-")}`);
   };
 
   return (
