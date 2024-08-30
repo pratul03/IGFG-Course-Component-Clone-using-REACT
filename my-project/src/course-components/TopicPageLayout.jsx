39edee0a4cd0741739dbc4b8ab234db42f6893bc
Pratul Makar <pratulmakar02@gmail.com>
2024-08-30 16:40:07 +0530

    key changes

---
+++ my-project/src/course-components/TopicPageLayout.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import TopicPage from "./TopicPage";
+
+const TopicPageLayout = () => {
+  return (
+    <div className="p-8">
+      <TopicPage />
+    </div>
+  );
+};
+
+export default TopicPageLayout;
