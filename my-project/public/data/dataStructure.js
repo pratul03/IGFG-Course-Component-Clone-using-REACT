ac73a52708ca820d5af861832311f1bece8f222f
Pratul Makar <pratulmakar02@gmail.com>
2024-09-09 03:09:43 +0530

    new

--- my-project/public/data/dataStructure.js
+++ my-project/public/data/dataStructure.js
@@ -1,34 +1,32 @@
 export const dataStructures = {
   title: "Learn Data Structures",
   description:
-    "Data structures are essential components that help organize and store data efficiently in computer memory. They provide a way to manage and manipulate data effectively, enabling faster access, insertion, and deletion operations. Common data structures include arrays, linked lists, stacks, queues, trees, and graphs, each serving specific purposes based on the requirements of the problem at hand. Understanding data structures is fundamental for designing efficient algorithms and optimizing software performance.",
+    "Data structures are essential components that help organize and store data efficiently in computer memory. They provide a way to manage and manipulate data effectively, enabling faster access, insertion, and deletion operations.",
+  description1:
+    " Common data structures include arrays, linked lists, stacks, queues, trees, and graphs, each serving specific purposes based on the requirements of the problem at hand. Understanding data structures is fundamental for designing efficient algorithms and optimizing software performance.",
+  heading: "Common Data Structures to learn : ",
   topics: [
     {
-      label: "Array Overview",
-      heading: "Learn About Arrays",
-      lastUpdated: "01 Sep, 2024",
-      boldText: "Arrays",
+      heading: "Array",
       description:
-        "An Array is a linear data structure that stores a collection of elements of the same data type in contiguous memory locations.",
-      content:
-        "Arrays provide constant-time access to elements, allowing for efficient operations like traversal, insertion, deletion, and searching.",
+        "Array is a linear data structure that stores a collection of elements of the same data type. Elements are allocated contiguous memory, allowing for constant-time access. Each element has a unique index number.",
       sections: [
         {
-          heading: "What is an Array?",
+          heading: "Operations on Array:",
           subHeading:
             "An Array is a linear data structure that stores a collection of elements of the same data type in contiguous memory locations.",
-          linkHead: "What is an Array?",
-          answerHead:
-            "Arrays provide constant-time access to elements, allowing for efficient operations like traversal, insertion, deletion, and searching.",
-          linkHead2: "How to use Arrays?",
-          answerHead2:
-            "Understanding arrays involves learning about operations such as traversal, insertion, deletion, and searching. Arrays come in various types like one-dimensional and multidimensional arrays.",
           answerLists: [
-            "Traversal: Iterating through the elements.",
-            "Insertion: Adding an element at a specific index.",
-            "Deletion: Removing an element at a specific index.",
-            "Searching: Finding an element by value or index.",
+            "Traversal: Iterating through the elements of an array.",
+            "Insertion: Adding an element to the array at a specific index.",
+            "Deletion: Removing an element from the array at a specific index.",
+            "Searching: Finding an element in the array by its value or index.",
           ],
+          heading2: "Types of Arrays:",
+          headSub: [
+            "One-dimensional array: A simple array with a single dimension.",
+            "Multidimensional array: An array with multiple dimensions, such as a matrix.",
+          ],
+          applicationsHeading: "Applications of Arrays:",
           applications: [
             "Storing data in a sequential manner",
             "Implementing queues, stacks, and other data structures",
@@ -43,25 +41,14 @@ export const dataStructures = {
       ],
     },
     {
-      label: "String Overview",
-      heading: "Learn About Strings",
-      lastUpdated: "01 Sep, 2024",
-      boldText: "Strings",
+      heading: "String",
       description:
-        "A string is a sequence of characters, typically used to represent text in computer programs.",
-      content:
-        "Strings allow for the manipulation and processing of textual data, including operations like concatenation, comparison, and substring extraction.",
+        "A string is a sequence of characters, typically used to represent text in computer programs. Strings allow for the manipulation and processing of textual data, including operations like concatenation, comparison, and substring extraction.",
       sections: [
         {
-          heading: "What is a String?",
+          heading: "Operations on String:",
           subHeading:
             "A string is a sequence of characters, typically used to represent text in computer programs.",
-          linkHead: "What is a String?",
-          answerHead:
-            "Strings allow for the manipulation and processing of textual data, including operations like concatenation, comparison, and substring extraction.",
-          linkHead2: "How to use Strings?",
-          answerHead2:
-            "Learn about operations such as concatenation, comparison, and search, as well as how to modify and extract substrings from strings.",
           answerLists: [
             "Concatenation: Joining two strings together.",
             "Comparison: Comparing two strings lexicographically.",
@@ -69,6 +56,7 @@ export const dataStructures = {
             "Search: Finding a substring within a string.",
             "Modification: Changing or replacing characters.",
           ],
+          applicationsHeading: "Applications of Strings:",
           applications: [
             "Text processing",
             "Pattern matching",
@@ -84,25 +72,14 @@ export const dataStructures = {
       ],
     },
     {
-      label: "Linked List Overview",
-      heading: "Learn About Linked Lists",
-      lastUpdated: "01 Sep, 2024",
-      boldText: "Linked Lists",
+      heading: "Linked List",
       description:
-        "A linked list is a linear data structure that stores data in nodes connected by pointers.",
-      content:
-        "Linked lists allow for dynamic resizing and efficient insertion or deletion of nodes at any position.",
+        "A linked list is a linear data structure that stores data in nodes connected by pointers. Linked lists allow for dynamic resizing and efficient insertion or deletion of nodes at any position.",
       sections: [
         {
-          heading: "What is a Linked List?",
+          heading: "Operations on Linked List:",
           subHeading:
             "A linked list is a linear data structure that stores data in nodes connected by pointers.",
-          linkHead: "What is a Linked List?",
-          answerHead:
-            "Linked lists allow for dynamic resizing and efficient insertion or deletion of nodes at any position.",
-          linkHead2: "How to use Linked Lists?",
-          answerHead2:
-            "Learn about creating, traversing, inserting, and deleting nodes in different types of linked lists, such as singly, doubly, and circular linked lists.",
           answerLists: [
             "Creation: Creating or adding nodes.",
             "Traversal: Accessing nodes sequentially.",
@@ -110,6 +87,7 @@ export const dataStructures = {
             "Deletion: Removing nodes from the list.",
             "Search: Finding nodes by value.",
           ],
+          applicationsHeading: "Applications of Linked Lists:",
           applications: [
             "Implementing queues and stacks",
             "Representing graphs and trees",
@@ -125,31 +103,21 @@ export const dataStructures = {
       ],
     },
     {
-      label: "Matrix/Grid Overview",
-      heading: "Learn About Matrices",
-      lastUpdated: "01 Sep, 2024",
-      boldText: "Matrices",
+      heading: "Matrix",
       description:
-        "A matrix is a two-dimensional array of elements arranged in rows and columns.",
-      content:
-        "Matrices represent data in a grid format, useful in various applications like image processing and data analysis.",
+        "A matrix is a two-dimensional array of elements arranged in rows and columns. Matrices represent data in a grid format, useful in various applications like image processing and data analysis.",
       sections: [
         {
-          heading: "What is a Matrix?",
+          heading: "Operations on Matrix:",
           subHeading:
             "A matrix is a two-dimensional array of elements arranged in rows and columns.",
-          linkHead: "What is a Matrix?",
-          answerHead:
-            "Matrices represent data in a grid format, useful in various applications like image processing and data analysis.",
-          linkHead2: "How to use Matrices?",
-          answerHead2:
-            "Learn about the concepts of rows, columns, and dimensions, and how to access elements using indices.",
           answerLists: [
             "Rows: Horizontal lines of elements.",
             "Columns: Vertical lines of elements.",
             "Dimensions: The size of the matrix in rows and columns.",
             "Element Access: Accessing elements using row and column indices.",
           ],
+          applicationsHeading: "Applications of Matrices:",
           applications: [
             "Image processing",
             "Data analysis",
@@ -164,25 +132,14 @@ export const dataStructures = {
       ],
     },
     {
-      label: "Stack Overview",
-      heading: "Learn About Stacks",
-      lastUpdated: "01 Sep, 2024",
-      boldText: "Stacks",
+      heading: "Stack",
       description:
-        "A Stack is a linear data structure that follows the LIFO (Last In First Out) principle.",
-      content:
-        "Stacks are used for operations like function calls, expression evaluation, and undo/redo operations.",
+        "A Stack is a linear data structure that follows the LIFO (Last In First Out) principle. Stacks are used for operations like function calls, expression evaluation, and undo/redo operations.",
       sections: [
         {
-          heading: "What is a Stack?",
+          heading: "Operations on Stack:",
           subHeading:
             "A Stack is a linear data structure that follows the LIFO (Last In First Out) principle.",
-          linkHead: "What is a Stack?",
-          answerHead:
-            "Stacks are used for operations like function calls, expression evaluation, and undo/redo operations.",
-          linkHead2: "How to use Stacks?",
-          answerHead2:
-            "Learn about stack operations such as push, pop, peek, and check if the stack is empty.",
           answerLists: [
             "Push: Adds an element to the top.",
             "Pop: Removes and returns the top element.",
@@ -190,6 +147,7 @@ export const dataStructures = {
             "Size: Gets the number of elements.",
             "IsEmpty: Checks if the stack is empty.",
           ],
+          applicationsHeading: "Applications of Stacks:",
           applications: [
             "Function calls",
             "Expression evaluation",
@@ -205,25 +163,14 @@ export const dataStructures = {
       ],
     },
     {
-      label: "Queue Overview",
-      heading: "Learn About Queues",
-      lastUpdated: "01 Sep, 2024",
-      boldText: "Queues",
+      heading: "Queue",
       description:
-        "A Queue is a linear data structure that follows the FIFO (First In First Out) principle.",
-      content:
-        "Queues are used in job scheduling, message queuing, and data buffering.",
+        "A Queue is a linear data structure that follows the FIFO (First In First Out) principle. Queues are used in job scheduling, message queuing, and data buffering.",
       sections: [
         {
-          heading: "What is a Queue?",
+          heading: "Operations on Queue:",
           subHeading:
             "A Queue is a linear data structure that follows the FIFO (First In First Out) principle.",
-          linkHead: "What is a Queue?",
-          answerHead:
-            "Queues are used in job scheduling, message queuing, and data buffering.",
-          linkHead2: "How to use Queues?",
-          answerHead2:
-            "Learn about queue operations such as enqueue, dequeue, front, and check if the queue is empty.",
           answerLists: [
             "Enqueue: Adds an element to the rear.",
             "Dequeue: Removes and returns the front element.",
@@ -231,6 +178,7 @@ export const dataStructures = {
             "Size: Gets the number of elements.",
             "IsEmpty: Checks if the queue is empty.",
           ],
+          applicationsHeading: "Applications of Queues:",
           applications: ["Job scheduling", "Message queuing", "Data buffering"],
           relatedTopics: [
             "Queue Tutorial",
@@ -241,25 +189,14 @@ export const dataStructures = {
       ],
     },
     {
-      label: "Graph Overview",
-      heading: "Learn About Graphs",
-      lastUpdated: "01 Sep, 2024",
-      boldText: "Graphs",
+      heading: "Graph",
       description:
-        "A graph is a non-linear data structure consisting of nodes (vertices) connected by edges.",
-      content:
-        "Graphs represent relationships between objects, enabling complex algorithms like shortest path finding and network analysis.",
+        "A graph is a non-linear data structure consisting of nodes (vertices) connected by edges. Graphs represent relationships between objects, enabling complex algorithms like shortest path finding and network analysis.",
       sections: [
         {
-          heading: "What is a Graph?",
+          heading: "Operations on Graph:",
           subHeading:
             "A graph is a non-linear data structure consisting of nodes (vertices) connected by edges.",
-          linkHead: "What is a Graph?",
-          answerHead:
-            "Graphs represent relationships between objects, enabling complex algorithms like shortest path finding and network analysis.",
-          linkHead2: "How to use Graphs?",
-          answerHead2:
-            "Learn about graph representations (adjacency matrix/list), traversal techniques (BFS, DFS), and common graph algorithms.",
           answerLists: [
             "Nodes: The entities or objects.",
             "Edges: Connections between nodes.",
@@ -268,6 +205,7 @@ export const dataStructures = {
             "BFS: Breadth-first search.",
             "DFS: Depth-first search.",
           ],
+          applicationsHeading: "Applications of Graphs:",
           applications: [
             "Social networks",
             "Routing algorithms",
@@ -283,25 +221,14 @@ export const dataStructures = {
       ],
     },
     {
-      label: "Tree Overview",
-      heading: "Learn About Trees",
-      lastUpdated: "01 Sep, 2024",
-      boldText: "Trees",
+      heading: "Tree",
       description:
-        "A tree is a hierarchical data structure consisting of nodes, with a root node and child nodes forming a parent-child relationship.",
-      content:
-        "Trees are used in various applications like database indexing, hierarchical data representation, and binary search.",
+        "A tree is a hierarchical data structure consisting of nodes, with a root node and child nodes forming a parent-child relationship. Trees are used in various applications like database indexing, hierarchical data representation, and binary search.",
       sections: [
         {
-          heading: "What is a Tree?",
+          heading: "Operations on Tree:",
           subHeading:
             "A tree is a hierarchical data structure consisting of nodes, with a root node and child nodes forming a parent-child relationship.",
-          linkHead: "What is a Tree?",
-          answerHead:
-            "Trees are used in various applications like database indexing, hierarchical data representation, and binary search.",
-          linkHead2: "How to use Trees?",
-          answerHead2:
-            "Learn about tree traversal methods (pre-order, in-order, post-order), binary trees, binary search trees, and AVL trees.",
           answerLists: [
             "Root: The topmost node.",
             "Child: A node connected to another node (parent).",
@@ -310,6 +237,7 @@ export const dataStructures = {
             "Binary Search Tree: A binary tree with ordered nodes.",
             "AVL Tree: A balanced binary search tree.",
           ],
+          applicationsHeading: "Applications of Trees:",
           applications: [
             "Database indexing",
             "Hierarchical data representation",
@@ -324,31 +252,21 @@ export const dataStructures = {
       ],
     },
     {
-      label: "Hashing Overview",
-      heading: "Learn About Hashing",
-      lastUpdated: "01 Sep, 2024",
-      boldText: "Hashing",
+      heading: "Hashing",
       description:
-        "Hashing is a technique used to map data to a fixed-size array or table through a hash function.",
-      content:
-        "Hashing enables fast data retrieval through direct access, commonly used in hash tables, dictionaries, and caches.",
+        "Hashing is a technique used to map data to a fixed-size array or table through a hash function. Hashing enables fast data retrieval through direct access, commonly used in hash tables, dictionaries, and caches.",
       sections: [
         {
-          heading: "What is Hashing?",
+          heading: "Operations on Hashing:",
           subHeading:
             "Hashing is a technique used to map data to a fixed-size array or table through a hash function.",
-          linkHead: "What is Hashing?",
-          answerHead:
-            "Hashing enables fast data retrieval through direct access, commonly used in hash tables, dictionaries, and caches.",
-          linkHead2: "How to use Hashing?",
-          answerHead2:
-            "Learn about hash functions, collision resolution techniques (chaining, open addressing), and applications of hashing.",
           answerLists: [
             "Hash Function: A function that maps data to a specific index.",
             "Collision: When two different inputs produce the same output.",
             "Chaining: A technique to handle collisions by linking entries.",
             "Open Addressing: A technique to handle collisions by probing.",
           ],
+          applicationsHeading: "Applications of Hashing:",
           applications: ["Hash tables", "Dictionaries", "Caches"],
           relatedTopics: [
             "Hashing Tutorial",
--- my-project/src/components/DataStructure.jsx
+++ my-project/src/components/DataStructure.jsx
@@ -1,58 +1,113 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import { dataStructures } from "../../public/data/dataStructure";
+import { Link } from "react-router-dom";
 
 const DataStructure = () => {
   return (
     <div className="flex flex-col w-full max-w-4xl p-6 text-white shadow-lg mt-3">
-      <h1 className="text-2xl font-medium text-green-600 underline hover:cursor-pointer mb-6">{dataStructures.title}</h1>
-      <p className="text-lg mb-8">{dataStructures.description}</p>
+      <h1 className="text-2xl font-medium text-green-600 underline hover:cursor-pointer mb-6">
+        {dataStructures.title}
+      </h1>
+      <p className="text-lg mb-4">{dataStructures.description}</p>
+      <p className="text-lg mb-8">{dataStructures.description1}</p>
+      <p className="text-xl font-semibold mb-8">{dataStructures.heading}</p>
 
-      {dataStructures.topics.map((topic, index) => (
-        <div key={index} className="mb-10">
-          <h2 className="text-3xl font-semibold mb-3">{topic.heading}</h2>
-          <p className="mt-2 text-sm text-gray-400">{topic.description}</p>
+      <ol className="list-decimal ml-2">
+        {dataStructures.topics.map((topic, index) => (
+          <li key={index} className="mb-10">
+            <h2 className="text-xl font-medium text-green-600 underline hover:cursor-pointer mb-3">
+              {topic.heading}
+            </h2>
+            <p className="mt-2 text-lg font-medium text-gray-100">
+              {topic.description}
+            </p>
 
-          {topic.sections.map((section, idx) => (
-            <div key={idx} className="mt-6 mb-6">
-              <h3 className="text-2xl font-bold mb-2">{section.heading}</h3>
-              <p className="text-xl font-medium">{section.subHeading}</p>
+            <ul className="list-disc ml-2">
+              {topic.sections.map((section, idx) => (
+                <li key={idx} className="mt-6 mb-6">
+                  <Link
+                    className="text-xl font-medium text-green-600 mb-2 underline"
+                    to="#"
+                  >
+                    {section.heading}
+                  </Link>
 
-              <div className="mt-4">
-                <h4>{section.linkHead}</h4>
-                <p>{section.answerHead}</p>
+                  <div className="mt-4">
+                    <ul className="list-[circle] ml-16 space-y-1 mt-3">
+                      {section.answerLists.map((item, listIdx) => {
+                        const [firstWord, ...rest] = item.split(": ");
+                        return (
+                          <li key={listIdx}>
+                            <span className="font-semibold">{firstWord}:</span>{" "}
+                            {rest.join(": ")}
+                          </li>
+                        );
+                      })}
+                    </ul>
+                  </div>
 
-                <h5 className="mt-2">{section.linkHead2}</h5>
-                <p>{section.answerHead2}</p>
+                  <div className="mt-4">
+                    <li>
+                      <Link
+                        className="
+                  text-xl font-medium text-green-600 underline hover:cursor-pointer mb-3"
+                      >
+                        {section.heading2}
+                      </Link>
+                    </li>
+                    <ul className="list-[circle] ml-16 space-y-1 mt-3">
+                      {section.headSub?.map((item, listIdx) => {
+                        const [firstWord, ...rest] = item.split(": ");
+                        return (
+                          <li key={listIdx}>
+                            <span className="font-semibold">{firstWord}:</span>{" "}
+                            {rest.join(": ")}{" "}
+                          </li>
+                        );
+                      })}
+                    </ul>
+                  </div>
 
-                <ul className="list-disc ml-5 space-y-1 mt-3">
-                  {section.answerLists.map((item, listIdx) => (
-                    <li key={listIdx}>{item}</li>
-                  ))}
-                </ul>
-              </div>
+                  <div className="mt-4">
+                    <li>
+                      <Link
+                        to="#"
+                        className="text-xl text-green-600 underline font-medium"
+                      >
+                        {section.applicationsHeading}
+                      </Link>
+                    </li>
+                    <ul className="list-[circle] ml-16 space-y-1">
+                      {section.applications.map((app, appIdx) => (
+                        <li key={appIdx}>{app}</li>
+                      ))}
+                    </ul>
+                  </div>
 
-              <div className="mt-4">
-                <h4 className="text-xl font-semibold">Applications:</h4>
-                <ul className="list-disc ml-5 space-y-1">
-                  {section.applications.map((app, appIdx) => (
-                    <li key={appIdx}>{app}</li>
-                  ))}
-                </ul>
-              </div>
-
-              <div className="mt-4">
-                <h4 className="text-xl font-semibold">Related Topics:</h4>
-                <ul className="list-disc ml-5 space-y-1">
-                  {section.relatedTopics.map((topic, topicIdx) => (
-                    <li key={topicIdx}>{topic}</li>
-                  ))}
-                </ul>
-              </div>
-            </div>
-          ))}
-        </div>
-      ))}
+                  <div className="mt-4">
+                    <li>
+                      <h4 className="text-lg font-semibold">Related Topics:</h4>
+                    </li>
+                    <ul className="list-[circle] ml-16 space-y-1">
+                      {section.relatedTopics.map((topic, topicIdx) => (
+                        <li key={topicIdx}>
+                          <Link
+                            className="text-green-600 underline font-medium"
+                            to="#"
+                          >
+                            {topic}
+                          </Link>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </li>
+        ))}
+      </ol>
     </div>
   );
 };
