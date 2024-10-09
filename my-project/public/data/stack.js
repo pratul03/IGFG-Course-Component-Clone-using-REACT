c384b0c97301c3624cefe7f6213a2eacce215199
Pratul Makar <pratulmakar02@gmail.com>
2024-10-09 23:49:27 +0530

    new

---
+++ my-project/public/data/stack.js
@@ -0,0 +1,36 @@
+export const stack = [
+  {
+    topics: [
+      {
+        title: "Stack Data Structure",
+        lastUpdated: "01 Sep, 2024",
+        description:
+          "A Stack is a linear data structure that follows a particular order in which the operations are performed. The order may be LIFO(Last In First Out) or FILO(First In Last Out). LIFO implies that the element that is inserted last, comes out first and FILO implies that the element that is inserted first, comes out last.",
+        image:
+          "https://media.geeksforgeeks.org/wp-content/cdn-uploads/20230726165552/Stack-Data-Structure.png",
+        sections: [
+          {
+            heading: "What is Stack Data Structure?",
+            description:
+              "A stack is a linear data structure that follows the Last-In-First-Out (LIFO) principle. It behaves like a stack of plates, where the last plate added is the first one to be removed.",
+            listHead: "Think of it this way:",
+            lists: [
+              "Pushing an element onto the stack is like adding a new plate on top.",
+              "Popping an element removes the top plate from the stack.",
+            ],
+          },
+          {
+            title: "Key Operations on Stack Data Structures",
+            listItem: [
+              "Push: Adds an element to the top of the stack.",
+              "Pop: Removes the top element from the stack.",
+              "Peek/Top: Returns the top element without removing it.",
+              "isEmpty: Checks if the stack is empty.",
+              "isFull: Checks if the stack is full (for fixed-size stacks).",
+            ],
+          },
+        ],
+      },
+    ],
+  },
+];
