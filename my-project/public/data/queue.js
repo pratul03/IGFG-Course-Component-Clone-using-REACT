79209f8cd355525f116c3ca434e81b55d7936151
Pratul Makar <pratulmakar02@gmail.com>
2024-10-10 01:11:14 +0530

    queue done ✅

---
+++ my-project/public/data/queue.js
@@ -0,0 +1,40 @@
+export const queue = [
+  {
+    topics: [
+      {
+        title: "Queue Data Structure",
+        lastUpdated: "01 Sep, 2024",
+        description:
+          "A Queue Data Structure is a fundamental concept in computer science used for storing and managing data in a specific order. It follows the principle of “First in, First out” (FIFO), where the first element added to the queue is the first one to be removed. Queues are commonly used in various algorithms and applications for their simplicity and efficiency in managing data flow.",
+        image:
+          "https://media.geeksforgeeks.org/wp-content/cdn-uploads/20230726165642/Queue-Data-structure1.png",
+        sections: [
+          {
+            heading: "What is Queue in Data Structures?",
+            content:
+              "A queue is a linear data structure that follows the First-In-First-Out (FIFO) principle. It operates like a line where elements are added at one end (rear) and removed from the other end (front).",
+          },
+          {
+            listHeading: "Basic Operations of Queue Data Structure",
+            listItems: [
+              "Enqueue (Insert): : Adds an element to the rear of the queue.",
+              "Dequeue (Delete): : Removes and returns the element from the front of the queue.",
+              "Peek: : Returns the element at the front of the queue without removing it.",
+              "Empty: :Checks if the queue is empty.",
+              "Full: : Checks if the queue is full.",
+            ],
+          },
+          {
+            listHeading: "Applications of Queue",
+            listItems: [
+              "Task scheduling: in operating systems",
+              "Data transfer: in network communication",
+              "Simulation: of real-world systems (e.g., waiting lines)",
+              "Priority queues: for event processing",
+            ],
+          },
+        ],
+      },
+    ],
+  },
+];
