e86e96bcd7dc51490df47b8b49daaf0f7a3b3b4b
Pratul Makar <pratulmakar02@gmail.com>
2024-10-10 13:08:40 +0530

    added a scroll toTop custom hook

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -3,12 +3,15 @@ import HomePageLayout from "./HomePageLayout";
 import CoursePageLayout from "./CoursePageLayout.jsx";
 // Reusable layout for course content
 import MainContent from "./sidebar/MainContent.jsx";
+import ScrollToTop from "./components/scrollToTop.jsx";
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Routes>
         {/* Main Home Layout */}
+
         <Route path="/" element={<HomePageLayout />} />
 
         {/* Dynamic Course Page */}
