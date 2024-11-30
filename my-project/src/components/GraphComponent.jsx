4c269f7848ec41087c02294120e3871f894d844a
Pratul Makar <pratulmakar02@gmail.com>
2024-12-01 01:19:18 +0530

    nnn

--- my-project/src/components/GraphComponent.jsx
+++ my-project/src/components/GraphComponent.jsx
@@ -21,19 +21,19 @@ const GraphComponent = () => {
 
   return (
     <div className="flex w-full mt-[-20px] ml-[-20px] flex-col">
-      <div className="left-0 flex flex-col w-[75%] max-w-5xl mr-auto p-6 text-white shadow-lg">
+      <div className="left-0 flex flex-col w-[75%] max-w-5xl mr-auto p-6 text-[#0e0e0e] shadow-lg">
         {graph[0]?.topics?.map((item, index) => (
           <div key={index} className="flex flex-col mb-4">
             {item.title && (
               <h3 className="text-2xl font-semibold mb-2">{item.title}</h3>
             )}
-            <p className="mt-2 text-sm font-semibold text-gray-200 flex items-center">
+            <p className="mt-2 text-sm font-semibold text-[#0e0e0e] flex items-center">
               {updateTime}
-              <div className="flex ml-auto gap-3 text-white relative">
+              <div className="flex ml-auto gap-3 text-[#0e0e0e] relative">
                 {/* MessageCircle Icon */}
                 <div className="relative group">
                   <MessageCircle className="h-5 cursor-pointer hover:text-gray-500" />
-                  <div className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-1 w-max p-1 bg-gray-500 text-gray-200 rounded opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-xs">
+                  <div className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-1 w-max p-1 bg-gray-500 text-[#0e0e0e] rounded opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-xs">
                     comments
                   </div>
                 </div>
@@ -41,7 +41,7 @@ const GraphComponent = () => {
                 {/* Pencil Icon */}
                 <div className="relative group">
                   <Pencil className="h-5 cursor-pointer hover:text-gray-500" />
-                  <div className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-1 w-max p-1 bg-gray-500 text-gray-200 rounded opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-xs">
+                  <div className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-1 w-max p-1 bg-gray-500 text-[#0e0e0e]rounded opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-xs">
                     improve
                   </div>
                 </div>
@@ -52,9 +52,9 @@ const GraphComponent = () => {
                 </div>
               </div>
             </p>
-            <hr className="border-t border-gray-200/30 my-2" />
+            <hr className="border-t border-gray-900 my-2" />
             {item.description && <p className="text-lg">{item.description}</p>}
-            <span className="flex flex-col justify-center content-center items-center">
+            <span className="flex flex-col justify-center content-center items-center bg-slate-300 rounded-lg">
               {item.image && (
                 <img
                   src={item.image}
